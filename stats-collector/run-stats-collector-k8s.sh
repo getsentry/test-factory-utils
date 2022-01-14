d59423860c5ba517420dadfcfbd6a5ad10e83234
Anton Ovchinnikov <anton@tonyo.info>
2022-01-14 16:49:30 +0100

    stats-collector: Update version

--- stats-collector/run-stats-collector-k8s.sh
+++ stats-collector/run-stats-collector-k8s.sh
@@ -2,7 +2,11 @@
 ### This script runs "stats-collector" on the default cluster
 set -euo pipefail
 
-IMAGE="europe-west3-docker.pkg.dev/sentry-st-testing/main/stats-collector:6d8216178208c4f2df812c6ddd8b3c9522b6242a"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${SCRIPT_DIR}"
+
+IMAGE_NAME="europe-west3-docker.pkg.dev/sentry-st-testing/main/stats-collector"
+IMAGE="$(grep ${IMAGE_NAME} ../k8s/services/workflows/z-run-test.yaml | awk '{print $2}')"
 INFLUX_URL=${INFLUX_URL:-http://influxdb-server.default.svc.cluster.local}
 
 # FIXME: arguments are not propagated properly because sentry-kube consumes '--'
