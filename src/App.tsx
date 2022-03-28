92b2ef820bdb12a5e5931467f5b6d701d39ffa64
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-03-28 19:39:27 +0200

    (WIP) added ky and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from "@mui/material/Button"
+import { useQuery} from "react-query";
+import ky from "ky"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getT1() : Promise<any>{
+    return ky.get("mock/t1.json").json()
 }
 
+
+function App() {
+    const  { isLoading, isError, data, error }  = useQuery('t1', getT1)
+
+    return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        <Button variant="contained">Hello Button</Button>
+                        <pre style={{"textAlign":"left"}}>
+                            {
+                                JSON.stringify(data, null, 2)
+                            }
+                    </pre>
+
+                    </p>
+
+
+                </header>
+            </div>
+    )
+}
 export default App;
