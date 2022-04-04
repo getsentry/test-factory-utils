632a324c347c4c3dbb825449f06756890be55a2e
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-04-04 10:33:30 +0200

    (WIP) Play with multi selector for filters

--- src/Search.tsx
+++ src/Search.tsx
@@ -9,6 +9,7 @@ import DateTimePicker from '@mui/lab/DateTimePicker';
 
 import React, {FocusEventHandler, KeyboardEventHandler, useState} from "react"
 import {ResultBrowserLocation, SearchParams} from "./location";
+import {MultipleSelectChip} from "./FilterSelector"
 
 type SearchActionType = "setLabel" | "setField"
 
@@ -124,6 +125,7 @@ export function Search() {
     })
     return (
         <>
+            <MultipleSelectChip/>
             <Box sx={{p: 2}}>
                 <ControlledTextBox fieldName="p11" label="P11 field" value={initialVal("p11")}
                                    setValue={updateLabel("p11")}/>
@@ -146,127 +148,4 @@ export function Search() {
             </Box>
         </>
     )
-}
-
-export function SearchOld() {
-    const search = useSearch<ResultBrowserLocation>()
-    const m = useMatch<ResultBrowserLocation>()
-
-
-    const [value, setValue] = useState<{ [key: string]: string }>(search?.labels ?? {});
-    const navigate = useNavigate()
-    const [fromDate, setFromDate] = React.useState<Date | null>(null);
-    const [toDate, settoDate] = React.useState<Date | null>(null);
-
-    const handleChange = (label: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event)
-        let val = event.target.value
-        setValue((old) => ({...old, [label]: val}))
-    };
-
-    const onKeyDown: (label: string) => KeyboardEventHandler<HTMLDivElement> = (label: string) => (e) => {
-        if (e.key === "Enter") {
-            console.log("fromDate on key down", e, value)
-
-            navigate({
-                search: (old: SearchParams | null | undefined) => {
-                    let labels = old?.labels ?? {}
-                    labels[label] = value[label]
-                    return {...old, labels}
-                }
-                ,
-                replace: true
-            })
-        }
-    }
-
-    const update: (label: string) => FocusEventHandler<HTMLInputElement> = (label: string) => (e) => {
-        navigate({
-            search: (old: SearchParams | null | undefined) => {
-                let labels = old?.labels ?? {}
-                labels[label] = value[label]
-                return {...old, labels}
-            }
-            ,
-            replace: true
-        })
-
-    }
-
-    return (<div>
-        <Box>This is the search page</Box>
-
-        <Box>The search params:</Box>
-        <Box sx={{p: 2}}>
-            <TextField
-                id="p1"
-                label="p1"
-                value={value.p1 ?? ""}
-                onChange={handleChange("p1")}
-                onKeyDown={onKeyDown("p1")}
-                onBlur={update("p1")}
-                variant="standard"
-            />
-        </Box>
-        <Box sx={{p: 2}}>
-            <TextField
-                id="p2"
-                label="p2"
-                value={value.p2 ?? ""}
-                onChange={handleChange("p2")}
-                onKeyDown={onKeyDown("p2")}
-                onBlur={update("p2")}
-                variant="standard"
-            />
-        </Box>
-        <RangePickerOld {...{toDate, fromDate, settoDate, setFromDate}}/>
-        <pre>
-            {JSON.stringify(search, null, 2)}
-        </pre>
-
-    </div>)
-}
-
-
-type SetStateOld = React.Dispatch<React.SetStateAction<Date | null>>
-
-
-interface RangePickerPropsOld {
-    toDate: Date | null;
-    fromDate: Date | null;
-    settoDate: SetStateOld;
-    setFromDate: SetStateOld;
-}
-
-function RangePickerOld(props: RangePickerPropsOld) {
-
-    return (<Box sx={{p: 2}}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Grid container columnSpacing={3}>
-                <Grid item>
-                    <Box sx={{p: 1}}>
-                        <DateTimePicker
-                            renderInput={(props: any) => <TextField {...props}/>}
-                            label="fromDate"
-                            value={props.fromDate}
-                            onChange={props.setFromDate}
-                        />
-                    </Box>
-
-                </Grid>
-                <Grid item>
-                    <Box sx={{p: 1}}>
-                        <DateTimePicker
-                            renderInput={(props: any) => <TextField {...props}/>}
-                            label="toDate"
-                            value={props.toDate}
-                            onChange={props.settoDate}
-                        />
-                    </Box>
-
-                </Grid>
-            </Grid>
-        </LocalizationProvider>
-    </Box>)
-
 }
