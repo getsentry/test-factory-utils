6ed10d968235eab78df68396718a31797a8d7be8
Anton Ovchinnikov <anton@tonyo.info>
2022-04-08 10:48:08 +0200

    update example (temp)

--- load-starter/examples/config.example.py
+++ load-starter/examples/config.example.py
@@ -23,31 +23,9 @@ CONFIG_VERSION = "1.0.0"
 LOCUST_URL = "http://0.0.0.0:8089"
 VEGETA_URL = "http://localhost:7770"
 
-def add_vegeta_tests():
-    add_vegeta_test(
-        duration=duration("30s"),
-        test_type="session",
-        name="The test",
-        description="Some test description",
-        freq=22,
-        per=duration("1s"),
-        config={
-            "startedRange": "1m",
-            "durationRange": "2m",
-            "numReleases": 3,
-            "numEnvironments": 4,
-            "numUsers": 5,
-            "okWeight": 6,
-            "exitedWeight": 7,
-            "erroredWeight": 8,
-            "crashedWeight": 9,
-            "abnormalWeight": 10
-        },
-
-    )
 
 def add_locust_tests():
-    for users in range( 20, 61, 20):
+    for users in range(20, 61, 20):
         add_locust_test(
             duration=duration("10s"),
             users=users,
@@ -55,12 +33,9 @@ def add_locust_tests():
             name="test with:%d users" % users,
             description="Some detailed description for test with %d users" % users,
         )
+        run_external(["/usr/bin/true"])
 
 
 # setup some locust tests
 set_load_tester_url(LOCUST_URL)
 add_locust_tests()
-
-# setup some vegeta tests
-set_load_tester_url(VEGETA_URL)
-add_vegeta_tests()
