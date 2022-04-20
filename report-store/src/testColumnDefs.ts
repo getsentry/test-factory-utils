c3f586677f86449a8f4f9e6fddca2233b84fdd9e
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-04-20 13:11:07 +0200

    messing with timezones (added luxon for this)

--- report-store/src/testColumnDefs.ts
+++ report-store/src/testColumnDefs.ts
@@ -1,5 +1,17 @@
-import {GridColDef, GridValueGetterParams} from "@mui/x-data-grid";
+import {GridColDef, GridValueFormatterParams, GridValueGetterParams} from "@mui/x-data-grid";
 import {getValue} from "./utils";
+import * as R from "rambda"
+import {DateTime} from "luxon"
+
+const DateFormatOptions:Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    //timeZoneName: "short",
+}
 
 const  getFromRow = (path:string)=>(params: GridValueGetterParams<any,any>)=> getValue(`row.${path}`, params)
 const  getDateFromRow = (path:string)=>(params: GridValueGetterParams<any,any>)=> {
@@ -9,6 +21,28 @@ const  getDateFromRow = (path:string)=>(params: GridValueGetterParams<any,any>)=
     } else{
         return null
     }
+}
+function dateFormater(params:GridValueFormatterParams<Date> ):string|Date{
+    if ( !params.value ){
+        return "-"
+    }
+    if ( !(params.value instanceof Date)){
+        return "invalid"
+    }
+
+    const dt = DateTime.fromJSDate(params.value, {zone: "utc"})
+    return dt.toLocaleString(DateFormatOptions)
+}
+
+function getCommentFromRow(params: GridValueGetterParams){
+    const val=  R.pipe(
+        getFromRow("context.parameters"),
+        R.find((x:any)=> x.name === "comment"),
+        R.path("value"),
+        (v:any)=> v ? v: "-",
+    )(params)
+    return val
+
 }
 
  const  TestColumns : GridColDef[] = [
@@ -20,12 +54,21 @@ const  getDateFromRow = (path:string)=>(params: GridValueGetterParams<any,any>)=
         type: "string",
     },
      {
-         field: "Start Date",
-         width: 300,
+         field: "Start Date (UTC)",
+         width: 200,
          hide: false,
          valueGetter: getDateFromRow("metadata.timeCreated.$date"),
+         valueFormatter: dateFormater,
          type: "dateTime",
-     }
+     },
+     {
+         field: "Comment",
+         flex: 1,
+         minWidth: 200,
+         hide: false,
+         valueGetter: getCommentFromRow,
+         type: "string",
+     },
 ]
 
 
