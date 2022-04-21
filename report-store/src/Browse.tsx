1f4e1a0445595ed049211889d90e3692813b792f
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-04-21 17:31:01 +0200

    clean unnecessary wrapping on browse page

--- report-store/src/Browse.tsx
+++ report-store/src/Browse.tsx
@@ -5,13 +5,12 @@ import ky from "ky"
 import {useNavigate, useSearch} from "@tanstack/react-location"
 import {useQuery} from "react-query"
 
-import {Box,Switch,Stack, Typography} from "@mui/material"
+import {Box, Switch, Stack, Typography} from "@mui/material"
 import {DataGrid} from "@mui/x-data-grid"
 
 import TestColumns from "./testColumnDefs"
 import {ResultBrowserLocation} from "./location"
 import {getValue, setValue} from "./utils"
-
 
 
 const Header: FunctionComponent<{}> = (props) => (
@@ -69,35 +68,21 @@ export function Browse() {
 
     return (
         <>
+            <Header>
+                <Stack direction="row" spacing={1} sx={{pl: 2}} alignItems="center">
+                    <Typography>Compare</Typography>
+                    <Typography sx={{color: compareOn ? "text.disabled" : "text.primary"}}>Off</Typography>
+                    <Switch
+                        checked={compareOn}
+                        onChange={handleChange}
+                        inputProps={{"aria-label": "controlled"}}
+                    />
+                    <Typography sx={{color: compareOn ? "text.primary" : "text.disabled"}}>On</Typography>
+                </Stack>
 
-            {/*<Header>*/}
-            {/*    <ControlledRangePicker*/}
-            {/*        label="Start run"*/}
-            {/*        toDate={getFromSearch("to")}*/}
-            {/*        fromDate={getFromSearch("from")}*/}
-            {/*        setToDate={(val) => updatePath("to", val)}*/}
-            {/*        setFromDate={(val) => updatePath("from", val)}*/}
-            {/*    />*/}
-            {/*</Header>*/}
+            </Header>
             <MainContent>
-                <Box style={{width: "100%", display: "flex", height: "100%", flexDirection: "column"}}>
-                    <Box sx={{flex: "0 0 auto"}}>
-
-                        <Stack direction="row" spacing={1}  sx={{pl:2}} alignItems="center">
-                            <Typography>Compare</Typography>
-                            <Typography sx={{color: compareOn ? "text.disabled": "text.primary"}}  >Off</Typography>
-                            <Switch
-                                checked={compareOn}
-                                onChange={handleChange}
-                                inputProps={{ "aria-label": "controlled" }}
-                            />
-                            <Typography sx={{color: compareOn ? "text.primary": "text.disabled"}}>On</Typography>
-                        </Stack>
-                    </Box>
-                    <Box sx={{flex: "1 1 auto"}}>
-                        <DataGrid checkboxSelection={compareOn} columns={TestColumns} getRowId={(row)=>getValue("name", row)} rows={isSuccess? reports: []} />
-                    </Box>
-                </Box>
+                <DataGrid checkboxSelection={compareOn} columns={TestColumns} getRowId={(row) => getValue("name", row)} rows={isSuccess ? reports : []}/>
             </MainContent>
         </>
     )
