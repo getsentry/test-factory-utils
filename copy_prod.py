9cd7cbe142adb3f1d12d20a0084d5d530c837ffb
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-06-27 20:02:48 +0200

    copy production functional

--- copy_prod.py
+++ copy_prod.py
@@ -9,11 +9,50 @@ In order to access the report-store endpoint you need to proxy into it
 after that you can get the two collections by calling the /api/reports endpoint
 specifying type=regular or type=sdk
 
-localhost:5800/api/reports?type=regular
-localhost:5800/api/reports?type=sdk
+localhost:5000/api/reports?type=regular
+localhost:5000/api/reports?type=sdk
 
 This will return a list of documents that can be inserted in the local mongodb
 
 """
+from pymongo import MongoClient
+import requests
+
+from utils import del_at
+
+URL = "http://localhost:5000"
+SDK_REPORT_COLLECTION = "sdk_report"
+REPORT_COLLECTION = "report"
 
 
+def main():
+    client = MongoClient("localhost", 27017)
+    db = client.main
+
+    resp = requests.get(f"{URL}/api/reports?type=regular")
+
+    if resp.status_code != 200:
+        print("Could not access reports server")
+
+    docs = resp.json()
+
+    add_docs(db[REPORT_COLLECTION], docs)
+
+    resp = requests.get(f"{URL}/api/reports?type=sdk")
+
+    if resp.status_code != 200:
+        print("Could not access reports server")
+
+    docs = resp.json()
+    add_docs(db[SDK_REPORT_COLLECTION], docs)
+
+
+def add_docs(collection, docs):
+    for doc in docs:
+        del_at(doc, "_id")
+
+    collection.insert_many(docs)
+
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -30,6 +30,13 @@ def get_at(obj, *args, **kwargs) -> Any:
     >>> get_at(x, "y.x", default="NOPE")
     'abc'
     """
+    path = _to_path(args)
+    default = kwargs.get("default")
+
+    return _get_at_path(obj, path, default)
+
+
+def _to_path(args) -> List[Union[str, int]]:
     path = []
     for arg in args:
         if isinstance(arg, int):
@@ -37,10 +44,70 @@ def get_at(obj, *args, **kwargs) -> Any:
         if isinstance(arg, str):
             ps = arg.split(".")
             path += ps
+    return path
 
-    default = kwargs.get("default")
 
-    return _get_at_path(obj, path, default)
+def set_at(obj, val, *args) -> bool:
+    """
+    >>> x = { "a":[{"x":{ "w": [1,2,3]}}], "y": {"x":"abc", "w":123}}
+    >>> set_at(x, 1, "x.w")
+    False
+    >>> set_at(x, 1, "a", 0, "x.w")
+    True
+    >>> get_at(x, "a.0.x.w")
+    1
+    >>> set_at(x, {"m": "n"}, "y.w")
+    True
+    >>> get_at(x, "y.w")
+    {'m': 'n'}
+
+    """
+    path = _to_path(args)
+    if len(path) < 1:
+        return False
+
+    sub_obj = _get_at_path(obj, path[:-1], None)
+
+    try:
+        if sub_obj is not None:
+            sub_obj[path[-1]] = val
+            return True
+    except:
+        return False
+    return False
+
+
+def del_at(obj, *args) -> bool:
+    """
+    >>> x = { "a":[{"x":{ "w": [1,2,3]}}], "y": {"x":"abc", "w":123}}
+    >>> del_at(x , "y.w")
+    True
+    >>> get_at(x , "y.w", default="Nope")
+    'Nope'
+    >>> del_at(x , "y")
+    True
+    >>> get_at(x, "y", default="Nope")
+    'Nope'
+    >>> del_at(x , "y")
+    False
+    >>> del_at(x, "a.0.x.w")
+    True
+    >>> get_at(x, "a.0.x")
+    {}
+
+    """
+    path = _to_path(args)
+    if len(path) < 1:
+        return False
+
+    sub_obj = _get_at_path(obj, path[:-1], None)
+    try:
+        if sub_obj is not None and path[-1] in sub_obj:
+            del sub_obj[path[-1]]
+            return True
+    except:
+        return False
+    return False
 
 
 def _get_at_path(obj, path: List[Union[str, int]], default) -> Any:
