ea40adfc9f84180a4caa25ea65485d5c7d015772
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-07-11 18:33:58 +0200

    minor fixes report spec

--- report-generator/report_spec.py
+++ report-generator/report_spec.py
@@ -94,6 +94,12 @@ class ConverterSpec:
     extra: Optional[str] = None
     func: Callable[[Any], Any] = None
 
+    @staticmethod
+    def from_dict(data):
+        type = data.get("type")
+        extra = data.get("extra")
+        return ConverterSpec(type=type, extra=extra)
+
 
 @dataclass
 class ValueExtractorSpec:
@@ -101,6 +107,18 @@ class ValueExtractorSpec:
     value: Optional[Any] = None
     compiledPath: Optional[ParsedResult] = None
     converter: Optional[ConverterSpec] = None
+
+    @staticmethod
+    def from_dict(data) -> "ValueExtractorSpec":
+        path = data.get("path")
+        value = data.get("value")
+        converter_dict = data.get("converter")
+        converter = None
+        if converter_dict is not None:
+            converter = ConverterSpec.from_dict(converter_dict)
+
+        # TODO finish implementation
+        return ValueExtractorSpec(path=path, value=value, converter=converter)
 
 
 @dataclass
@@ -117,7 +135,7 @@ class RowExtractorSpec:
         accepts_null = data.get("accepts_null", False)
         columns_raw = data.get("columns", [])
         columns = [ValueExtractorSpec.from_dict(column_raw) for column_raw in columns_raw]
-        return ValueExtractorSpec(accepts_null=accepts_null, columns=columns)
+        return RowExtractorSpec(accepts_null=accepts_null, columns=columns)
 
 
 @dataclass
@@ -128,7 +146,7 @@ class DataFrameSpec:
     # None or one of  the pandas types specified as a string: "float, int, str, datetime[ns/ms/s...],timestamp[ns/ms...] ; only necessary for datetime
     column_types: Optional[List[Optional[str]]] = None
     mongo_filter: Any = None
-    mongo_sort: Any = None # mongo db cursor sort (use it if you can)
+    mongo_sort: Any = None  # mongo db cursor sort (use it if you can)
     mongo_projection: Any = None
     dataframe_sort: Optional[str] = None  # sort the dataframe by column (use it if you can't use mongo sort), use -column to sort descending
 
