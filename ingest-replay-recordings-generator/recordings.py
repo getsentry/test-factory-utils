a148ca96e89da1a56e93b8368c0ff02ed26f7899
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2023-08-16 04:54:59 -0400

    feat(recording): use actual recording segments for generator (#84)

--- ingest-replay-recordings-generator/recordings.py
+++ ingest-replay-recordings-generator/recordings.py
@@ -1,15 +1,31 @@
 from typing import Mapping, Any
 import zlib
+import random
 import time
 
+from message_types import XSMALL_MESSAGE, SMALL_MESSAGE, MEDIUM_MESSAGE, LARGE_MESSAGE
+
+def _get_message_size():
+    n = random.randint(0, 100)
+    if n < 50:
+        # 236 bytes compressed
+        return XSMALL_MESSAGE
+    if n < 80:
+        # 2KB compressed
+        return SMALL_MESSAGE
+    if n < 95:
+        # 25KB compressed
+        return MEDIUM_MESSAGE
+    if n <= 100:
+        # 162KB compressed
+        return LARGE_MESSAGE
 
 def generate_message(
     replay_id: str,
     segment_id: int,
     settings: Mapping[str, Any]
 ):
-    bytes_msg = bytes(settings["message"], "utf-8")
-    message = zlib.compress(bytes_msg) if settings["compressed"] else bytes_msg
+    message = zlib.compress(_get_message_size())
     return {
         "type": "replay_recording_not_chunked",
         "replay_id": replay_id,
