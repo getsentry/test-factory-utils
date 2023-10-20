f21f80f12ec67a2ca0d6755dc4e58522bfb41c46
Sebastian Zivota <loewenheim@users.noreply.github.com>
2023-10-20 13:47:00 +0200

    feat: Add ingest-attachments-generator (#87)
    * feat: Add ingest-attachments-generator
    
    This adds a new message generator that sends a
    number of attachments.

---
+++ ingest-attachments-generator/main.py
@@ -0,0 +1,194 @@
+from typing import Optional
+
+import click
+from confluent_kafka import Producer
+from yaml import load, Loader
+
+from messages import generate_event_messages, generate_attachment_payloads
+from readme_generator import generate_readme
+
+
+@click.command()
+@click.option(
+    "--num-attachments",
+    "-n",
+    default="",
+    help="The number of attachments to send to the kafka queue",
+)
+@click.option(
+    "--num-payloads",
+    "-p",
+    default="",
+    help="The number of different attachment payloads to send to the kafka queue",
+)
+@click.option(
+    "--settings-file", "-f", default=None, help="The settings file name (json or yaml)"
+)
+@click.option(
+    "--topic-name", "-t", default=None, help="The name of the ingest metrics topic"
+)
+@click.option(
+    "--broker",
+    "-b",
+    default=None,
+    help="Kafka broker address and port (e.g. localhost:9092)",
+)
+@click.option(
+    "--timestamp",
+    type=int,
+    help="Timestamp reference to use. If exactly repeatable tests are desired then a timestamp ref can be used",
+)
+@click.option(
+    "--spread",
+    "-s",
+    help="Time spread from timestamp backward, (e.g. 1w2d3h4m5s 1 week 2 days 3 hours 4 minutes 5 seconds)",
+)
+@click.option("--org", "-o", type=int, help="organization id")
+@click.option("--project", "-p", type=int, help="project id")
+@click.option("--dry-run", is_flag=True, help="if set only prints the settings")
+@click.option("--update-docs", is_flag=True,  help="creates a README.md  documentation file")
+def main(**kwargs):
+    """
+    Populates the ingest-metrics kafka topic with messages
+    """
+    if kwargs["update_docs"]:
+        generate_readme()
+        return
+
+    settings = get_settings(**kwargs)
+
+    print("Settings:")
+    print(settings)
+
+    if kwargs["dry_run"]:
+        return
+
+    producer = get_kafka_producer(settings)
+
+    print("Generating attachment data...", flush=True)
+
+    payloads = generate_attachment_payloads(settings)
+
+    print("Sending data...", flush=True)
+    send_messages(producer, settings, payloads)
+
+    print("Done!")
+
+
+def send_messages(producer, settings, payloads):
+    topic_name = settings["topic_name"]
+    num_attachments = settings["num_attachments"]
+
+    for idx in range(num_attachments):
+        for message in generate_event_messages(idx, settings, payloads[idx % len(payloads)]):
+            producer.produce(topic_name, message)
+            producer.poll(0)
+
+    producer.flush()
+
+
+def get_settings(
+    num_attachments: Optional[str],
+    num_payloads: Optional[str],
+    settings_file: Optional[str],
+    topic_name: Optional[str],
+    broker: Optional[str],
+    org: Optional[int],
+    project: Optional[int],
+    dry_run: bool,
+    **kwargs,
+):
+    # default settings
+    settings = {
+        "num_attachments": 100,
+        "num_payloads": 1,
+        # attachments is the most defensive default, since this topic allows all
+        # message types.
+        "topic_name": "ingest-attachments",
+        "kafka": {},
+        "metric_types": {},
+    }
+
+    if settings_file is not None:
+        try:
+            with open(settings_file, "rt") as f:
+                settings.update(load(f, Loader=Loader))
+        except:
+            raise click.UsageError(f"Could not parse settings file {settings_file}")
+
+    if topic_name is not None:
+        settings["topic_name"] = topic_name
+
+    if broker is not None:
+        settings["kafka"]["bootstrap.servers"] = broker
+
+    if org is not None:
+        settings["org"] = org
+
+    if project is not None:
+        settings["projects"] = [project]
+
+    if settings["kafka"].get("bootstrap.servers") is None:
+        raise click.UsageError(
+            f"Kafka broker was not specified, to specify either use --broker argument or set [kafka][bootstrap.servers] in the settings file"
+        )
+
+    if settings.get("org") is None:
+        raise click.UsageError(
+            f"Organization was not specified, to specify either use --org argument or set [org] in the settings file"
+        )
+
+    if settings.get("projects") is None:
+        raise click.UsageError(
+            f"projects not specified, to specify either use --project argument or set [project] array in the settings file"
+        )
+
+    # num_attachments may be set to a string value (when started from kubernetes) like 'default'
+    # if set in the command line to anything that can't be converted to an integer just ignore it
+    for name, value in (
+        ("num_attachments", num_attachments),
+        # NB: Add other numeric settings here
+    ):
+        if value is not None:
+            try:
+                settings[name] = int(value)
+            except ValueError:
+                pass  # ignore non integer command line args
+
+    settings["dry_run"] = dry_run
+
+    return settings
+
+
+def get_fake_kafka_producer(settings):
+    """
+    A fake producer that just dumps to console (for testing)
+    """
+
+    class FakeProducer:
+        def __init__(self, settings):
+            pass
+
+        def produce(self, topic_name, message):
+            print(message)
+
+        def flush(self):
+            print("Flushing !!! ")
+
+    return FakeProducer(settings)
+
+
+def get_kafka_producer(settings):
+    """
+    Returns a kafka producer configured with the
+    settings found in the settings["kafka"] sub-object
+
+    At a minimum the settings should contain:
+        bootstrap.server: host-name:port-number
+    """
+    kafka_settings = settings["kafka"]
+    return Producer(kafka_settings)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ingest-attachments-generator/messages.py
@@ -0,0 +1,69 @@
+import random
+from typing import Mapping, Any, Optional, List
+import uuid
+
+import msgpack
+
+
+def generate_attachment_payloads(settings: Mapping[str, Any]) -> List[bytes]:
+    payloads = []
+    # TODO: Make min/max size configurable?
+    for i in range(settings["num_payloads"]):
+        payloads.append("".join([chr(random.randint(0, 255)) for _ in range(100)]).encode("utf-8"))
+    return payloads
+
+
+def generate_event_messages(idx: int, settings: Mapping[str, Any], payload: bytes):
+    event_id = _get_event_id(idx, settings)
+    attachment_id = _get_attachment_id(idx, settings)
+    project_id = _get_project_id(idx, settings)
+
+    chunk_headers = {
+        "event_id": event_id,
+        "project_id": project_id,
+        "id": attachment_id,
+        "chunk_index": 0,  # TODO: Split file into multiple chunks?
+    }
+
+    yield _create_msgpack_wrapper("attachment_chunk", chunk_headers, payload)
+
+    attachment_headers = {
+        "event_id": event_id,
+        "project_id": project_id,
+        "attachment": {
+            "id": attachment_id,
+            "name": "test.txt",
+            "content_type": "text/plain",
+            "attachment_type": "event.attachment",
+            "chunks": 1,
+            "size": 1000,
+            "rate_limited": False,
+        }
+    }
+
+    yield _create_msgpack_wrapper("attachment", attachment_headers, None)
+
+
+def _create_msgpack_wrapper(ty: str, headers: Mapping[str, Any], payload: Optional[bytes]) -> bytes:
+    message = {**headers, "type": ty}
+
+    if payload is not None:
+        message["payload"] = payload
+
+    return msgpack.packb(message)
+
+
+def _get_project_id(idx: int, settings: Mapping[str, Any]) -> int:
+    projects = settings["projects"]
+
+    proj_idx = random.randint(0, len(projects) - 1)
+    return projects[proj_idx]
+
+
+def _get_event_id(idx: int, settings: Mapping[str, Any]) -> str:
+    return uuid.uuid4().hex
+
+
+def _get_attachment_id(idx: int, settings: Mapping[str, Any]) -> str:
+    return uuid.uuid4().hex
+
